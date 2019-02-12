959bd6fc4654853655de70aa2546f684743f35b5
Yeshwanth M <yeshwanth.m@seventablets.com>
2019-02-12 15:34:46 -0600

    Add custom cluster dataset method

--- clustering/iris.py
+++ clustering/iris.py
@@ -1,15 +1,22 @@
 import pandas as pd
 from sklearn import datasets
+from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import sys
 sys.path.append('../')
 from scikit_clustering import k_means, get_optimal_cluster_count, pca_transform, get_pca_comp_count, get_clustering_scores
 
+
+X, y = make_blobs(n_samples=10000, centers=2, n_features=2,
+                  random_state=0)
+data = X
+target = y
+
 # dataset = datasets.load_iris()
-dataset = datasets.load_wine()
-data = dataset.data
-target = dataset.target
+# dataset = datasets.load_wine()
+# data = dataset.data
+# target = dataset.target
 
 optimal_count = get_optimal_cluster_count(data_frame=data)
 get_clustering_scores(n_clust=optimal_count,
